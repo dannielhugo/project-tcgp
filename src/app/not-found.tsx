81cf36f3a5e0cb4ab2cbff75b34f7ed79531f77c
Danniel Hugo <dannielhugo@gmail.com>
2025-06-06 09:41:09 -0300

    feat: add base pages

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <h2>Not Found: </h2>
+      <p>Could not find requested resource</p>
+    </main>
+  );
+}
