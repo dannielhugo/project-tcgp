d4bd9ecf3924d4f76a38668e76b3cc3aa89a0f43
Danniel Hugo <dannielhugo@gmail.com>
2025-07-03 18:08:21 -0300

    build: ignore prisma generated folder on lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,10 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.config({
+    ignores: ['app/generated/prisma/*'],
+  }),
 ];
 
 export default eslintConfig;
