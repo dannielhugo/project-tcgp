2c2823068ae29974f9625f9d92cce955f55ba482
Danniel Hugo <dannielhugo@gmail.com>
2025-07-03 18:15:06 -0300

    chore: fix lint ignore prisma

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { globalIgnores } from 'eslint/config';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,9 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  ...compat.config({
-    ignores: ['app/generated/prisma/*'],
-  }),
+  globalIgnores(['app/generated/prisma/*'], 'Ignore Prisma Directory'),
 ];
 
 export default eslintConfig;
